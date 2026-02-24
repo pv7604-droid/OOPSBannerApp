1d20736fdb4727b5977278f10c82ba4c57d7c873
pv7604-droid <pv7604@srmist.edu.in>
2026-02-24 11:15:02 +0530

    ok

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp - UC1: Print literal text "OOPS"
+ * @author Prayag Shankar Vinod
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    
+    /**
+     * Main entry point of the application.
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // Printing the literal text "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
+
+    
